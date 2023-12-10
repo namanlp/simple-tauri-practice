64be9eea35534c7c42a0dcbefcaa47138d3dbec7
namanlp <naman@garudalinux.org>
2023-12-10 22:27:19 +0530

    Enable devtools

--- calculator-example/src-tauri/src/main.rs
+++ calculator-example/src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn calculate_result(expression1 : String, expression2 : String, operator: char) -> f64 {
@@ -20,10 +22,16 @@ fn calculate_result(expression1 : String, expression2 : String, operator: char) 
 }
 
 
-
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![calculate_result])
-        .run(tauri::generate_context!())
+    #[cfg(debug_assertions)] // only enable instrumentation in development builds
+    let devtools = devtools::init();
+
+    let builder = tauri::Builder::default();
+
+    #[cfg(debug_assertions)]
+        let builder = builder.plugin(devtools);
+
+    builder
+        .run(tauri::generate_context!("./tauri.conf.json"))
         .expect("error while running tauri application");
 }
